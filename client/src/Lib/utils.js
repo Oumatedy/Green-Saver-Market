315828fd6baa1e26fda8d6cfb2d2bc6436cc99f8
Oumatedy <oumatedy@gmail.com>
2025-07-30 18:45:54 +0300

    feat: updated lib/util for deployment

--- client/src/Lib/utils.js
+++ client/src/Lib/utils.js
@@ -1,11 +1,7 @@
-// import { clsx } from "clsx";
-// import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge"
 
-// export function cn(...inputs) {
-//   return twMerge(clsx(inputs));
-// }
-
-export function cn(...args) {
-  return args.filter(Boolean).join(' ');
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
 }
 
